dd4dcdd942dba69bfef362a554135f9832a45049
Кристина <kristina.shtaynmets@mail.ru>
2023-10-31 10:50:05 +0700

    Добавляем новый класс и регистрируем его

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -13,3 +13,15 @@ class AuthorAdmin(admin.ModelAdmin):
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
+
+# Register the Admin classes for Book using the decorator
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+# Register the Admin classes for BookInstance using the decorator
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
