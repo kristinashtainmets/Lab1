cd92193fb2302a4fab8408b09196bdbdc0bfad2b
Кристина <kristina.shtaynmets@mail.ru>
2023-10-31 18:14:34 +0700

    Обновляем библиотеку, чтобы сообщать пользователю количество визитов

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -14,15 +14,17 @@ def index(request):
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.count() # Метод 'all()' применён по умолчанию.
     num_genres=Genre.objects.count()
-
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors,'num_genres':num_genres},
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors,'num_genres':num_genres,'num_visits':num_visits},
     )
+
 
 from django.views import generic
 
