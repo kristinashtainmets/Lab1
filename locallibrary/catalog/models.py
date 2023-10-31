cb05e0ae6a53896385815bf567253960d2ca623e
Кристина <kristina.shtaynmets@mail.ru>
2023-10-31 20:24:56 +0700

    Проверка на срок сдачи книги

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -50,6 +50,8 @@ class Book(models.Model):
     display_genre.short_description = 'Genre'
 
 import uuid # Required for unique book instances
+from datetime import date
+from django.contrib.auth.models import User
 
 class BookInstance(models.Model):
     """
@@ -59,6 +61,7 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -78,6 +81,12 @@ class BookInstance(models.Model):
         String for representing the Model object
         """
         return '%s (%s)' % (self.id,self.book.title)
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
 class Author(models.Model):
     """
