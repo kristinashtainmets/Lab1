9c1a411a2e975a26450d84f2a5ba3381268e5a6b
Кристина <kristina.shtaynmets@mail.ru>
2023-10-31 11:24:33 +0700

    Дополняем класс BookInstanceAdmin

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -25,3 +25,11 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
